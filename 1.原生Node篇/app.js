6c904384687164c43b1b2a7e241fec9c537b744b
yongyangwu <yongyang.wu@outlook.com>
2018-03-01 06:53:30 +0800

    from windows

--- 1.原生Node篇/app.js
+++ 1.原生Node篇/app.js
@@ -6,8 +6,8 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
     // 发送响应数据 "Hello World"
     response.end("收到");
-}).listen(8888,function () {
+}).listen(8388,function () {
     // 终端打印如下信息
-    console.log('Server running at http://127.0.0.1:8888/');
+    console.log('Server running at http://127.0.0.1:8388/');
 });
 
