fc88bb6141d16063a20590bad7cc5415cee4bb1e
yongyangwu <yongyang.wu@outlook.com>
2018-03-16 09:17:34 +0800

    from windows

--- 2.node-cookie/app2.js
+++ 2.node-cookie/app2.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var app = express();
-// app.get("/",function (req,res) {
-//     //设置cookie
-//     // res.cookie('xihao','tfboys',{ maxAge:900000, httpOnly:true });
-//     //读取cookies
-//     // console.log(req.cookies);
-//     res.send("设置cookie成功:");
-// });
 app.get("/",function (req,res) {
     console.log(req.ip);
     res.send("hello world");
 });
-
 app.listen(3000,function () {
     console.log("server is running");
 });
